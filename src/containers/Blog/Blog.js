238454124d0274d9e53952364ba90aef43833f3c
Issouf Zabre <sheridanzabre@gmail.com>
2021-03-04 09:40:03 -0500

    transform data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import axios from 'axios';
 
 class Blog extends Component {
     state ={
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount () {
@@ -24,10 +25,20 @@ class Blog extends Component {
                 this.setState({posts: updatedPosts});
             });
     }
+
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id})
+
+    }
     
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author} />
+            return <Post
+             key={post.id}
+             title={post.title}
+             author={post.author}
+             clicked={() => this.postSelectedHandler(post.id)} />
         })
         return (
             <div>
@@ -35,7 +46,7 @@ class Blog extends Component {
                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
