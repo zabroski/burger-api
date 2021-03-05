24273fda4860c09f93d4dd8a6dac39418f52ab27
Issouf Zabre <sheridanzabre@gmail.com>
2021-03-05 14:06:30 -0500

    created  axios instance Please enter the commit message for your changes. Lines starting

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,7 +4,8 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
-import axios from 'axios';
+// import axios from 'axios';
+import axios from '../../axios'
 
 class Blog extends Component {
     state ={
@@ -13,8 +14,10 @@ class Blog extends Component {
         error: false
     }
 
+    
+
     componentDidMount () {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('/posts')
             .then(response => {
                 const posts = response.data.slice(0,4);
                 const updatedPosts = posts.map(post => {
