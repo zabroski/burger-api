015cd8943ac61decf2c3dddb7959d3efcbbbb368
Issouf Zabre <sheridanzabre@gmail.com>
2021-03-05 13:07:23 -0500

    handle error

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import axios from 'axios';
 class Blog extends Component {
     state ={
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount () {
@@ -23,6 +24,9 @@ class Blog extends Component {
                     }
                 })
                 this.setState({posts: updatedPosts});
+            })
+            .catch(error => {
+                this.setState({error: true});
             });
     }
 
@@ -33,13 +37,17 @@ class Blog extends Component {
     }
     
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post
-             key={post.id}
-             title={post.title}
-             author={post.author}
-             clicked={() => this.postSelectedHandler(post.id)} />
-        })
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
+        if (!this.state.error) {
+             posts = this.state.posts.map(post => {
+                return <Post
+                 key={post.id}
+                 title={post.title}
+                 author={post.author}
+                 clicked={() => this.postSelectedHandler(post.id)} />
+            });
+        }
+     
         return (
             <div>
                 <section className="Posts">
